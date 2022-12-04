ba9123f300c1ff0830961dd97068a214dd58ae07
Florian Keider <me@altbrot.at>
2022-12-04 16:47:10 +0100

    load data from executing assembly.

--- 2022/Altbrot.AoC.2022.01/Program.cs
+++ 2022/Altbrot.AoC.2022.01/Program.cs
@@ -2,12 +2,15 @@
 using Altbrot.AoC._2022._01.Application.ElfBags.UseCases;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 var host = CreateHostBuilder(args).Build();
 
 var service = host.Services.GetRequiredService<IGetHighestAmountOfCaloriesPerElf>();
 
-var data = File.ReadAllText("data.txt");
+var data = File.ReadAllText(Path.Combine(
+    Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!,
+    "data.txt"));
 var highestAmount = await service.ExecuteAsync(data);
 
 Console.WriteLine($"The answer is {highestAmount}");
